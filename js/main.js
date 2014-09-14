df7f66a4165130c287d864827504b76a18453f70
zohairHemani1 <zohairhemani1@gmail.com>
2014-09-14 05:31:44 +0500

    Login Functionality

--- js/main.js
+++ js/main.js
@@ -6,20 +6,45 @@ $(document).ready(function(e) {
 
 
 function bindElements() {
-	$(document).on("click", "#login", login);
+	$(document).on("click", "#login_button", login);
 	$(document).on("click", "#register", register);
 }
 
 function login()
 {
-	alert('Login');
+	
+	var success = function(response) 
+	{
+		response = JSON.parse(response);
+		var successMsg;
+		console.log(JSON.stringify(response));
+		if(response.success != null) 
+		{
+			successMsg = response.success;
+			alert(successMsg);
+		}
+		else {
+			successMsg = response.error;
+			alert(successMsg);
+		}
+    } 
+	
+	var failed = function(response) {
+		alert('failed');
+		console.log(JSON.stringify(response));
+	}
+
+	var jsonHelper = new ServiceHelper();
+	jsonHelper.loginUser(JSON_CONSTANTS.POST, '#loginForm' , success, failed);
+		
+	
+	
+	
 }
 
 function register()
 {
-	
-	alert('Register');
-	
+		
 	var email = $('#email').val();	
 	var pass = $('#password').val();
 	var verifyPass = $('#verifyPassword').val();
@@ -37,7 +62,6 @@ function register()
 			alert(successMsg);
 		}
 		else {
-			//
 			successMsg = response.error;
 			alert(successMsg);
 		}
