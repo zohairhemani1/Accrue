c784a13651524285d0c689f6086d9972d7dab256
zohairHemani1 <zohairhemani1@gmail.com>
2014-10-03 16:01:11 +0500

    Query Optimized. 24 Hours.

--- www/WebServices/getTotal.php
+++ www/WebServices/getTotal.php
@@ -8,18 +8,17 @@ include 'headers/connect_database.php';
 	 $returnArray = array();
 	 $user_id = $_GET['user_id'];
 	 
-	 $query = "SELECT sum(`amount`) AS sum FROM `expenditure` group by `email_id` HAVING `email_id` = '$user_id'";
+	 $query = "SELECT sum(`amount`) AS sum, count(*) AS count FROM `expenditure` 
+	 				WHERE timestamp > DATE_ADD(NOW(), INTERVAL -24 HOUR) 
+					group by `email_id` HAVING `email_id` = '$user_id'";
+					
 	 $result = mysqli_query($con,$query);
 	 $row = mysqli_fetch_assoc($result);
 	 $totalExpenditure = $row['sum'];
+	 $totalCount = $row['count'];
 	 
 	 $returnArray['totalExpenditure'] = $totalExpenditure;
-	 
-	 $query = "SELECT * FROM `expenditure` WHERE email_id = '$user_id'";
-	 $result = mysqli_query($con,$query);
-	 $count = mysqli_num_rows($result);
-	 
-	 $returnArray['expenditureCount'] = $count;
+	 $returnArray['expenditureCount'] = $totalCount;
 	 
 	 
 	 echo json_encode($returnArray);
